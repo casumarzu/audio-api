64551f044f2e789e93e19a2e6228f997e03820e0
andrey <casumarzu@yandex.ru>
2016-08-24 14:04:57 +0300

    Change code

--- src/scripts/containers/App.js
+++ src/scripts/containers/App.js
@@ -1,10 +1,12 @@
 import React, { Component, PropTypes } from 'react'
-import { Router, Route, Link, browserHistory } from 'react-router'
-import { bindActionCreators } from 'redux'
-import { connect } from 'react-redux'
+import {Router, Route, Link, browserHistory} from 'react-router'
+import {bindActionCreators} from 'redux'
+import {connect} from 'react-redux'
 import DevTools from 'Containers/DevTools'
-import { getMuiTheme, MuiThemeProvider } from 'material-ui/styles'
-import { muiStyle } from 'Scripts/config'
+import {getMuiTheme, MuiThemeProvider} from 'material-ui/styles'
+import {muiStyle} from 'Scripts/config'
+
+// import {firebaseApp, firebaseAuth} from 'Api/Firebase'
 
 import Header from 'Layout/Header'
 import Footer from 'Layout/Footer'
